5e7f2af1c1588d187d14eab16ec8b71bf6306774
Christian Spilhere <cspilhere@gmail.com>
2017-11-03 07:54:59 -0200

    Ajuste na função uuid

--- src/uuid.js
+++ src/uuid.js
@@ -1,5 +1,6 @@
 export default function uuid() {
   let time = () => new Date();
   let uuid = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + time().getTime();
+  //
   return uuid();
 };
